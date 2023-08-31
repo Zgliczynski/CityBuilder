1e06da4bfd340973e1e417bcc7b5447c36abfbb4
Zgliczynski <zgliczynskikrzysztof@gmail.com>
2023-08-31 22:17:57 +0200

    input & game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using SVS;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public CameraMovement cameraMovement;
+
+    public InputManager inputManager;
+
+    private void Start()
+    {
+        inputManager.OnMouseClick += HandleMouseClick;
+    }
+
+    private void HandleMouseClick(Vector3Int position)
+    {
+        Debug.Log(position);
+    }
+
+    private void Update()
+    {
+        cameraMovement.MoveCamera(new Vector3(inputManager.CameraMovementVector.x, 0, inputManager.CameraMovementVector.y));
+    }
+}
